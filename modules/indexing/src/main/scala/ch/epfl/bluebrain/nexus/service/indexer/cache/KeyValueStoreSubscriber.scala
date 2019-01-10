181d8ef13a0a353fe4becd5aa58a61201aa662ec
Didac Montero <didac.monteromendez@epfl.ch>
2019-01-10 21:42:42 +0100

    Added subscriber notifications

---
+++ modules/indexing/src/main/scala/ch/epfl/bluebrain/nexus/service/indexer/cache/KeyValueStoreSubscriber.scala
@@ -0,0 +1,111 @@
+package ch.epfl.bluebrain.nexus.service.indexer.cache
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.cluster.ddata.LWWMapKey
+import akka.cluster.ddata.Replicator.Changed
+import ch.epfl.bluebrain.nexus.service.indexer.cache.KeyValueStoreSubscriber.KeyValeStoreChange._
+import ch.epfl.bluebrain.nexus.service.indexer.cache.KeyValueStoreSubscriber._
+
+trait OnKeyValueStoreChange[K, V] {
+
+  /**
+    * Method that gets triggered when a change to key value store occurs.
+    *
+    * @param value the changes made
+    */
+  def apply(value: KeyValeStoreChanges[K, V]): Unit
+}
+
+object OnKeyValueStoreChange {
+  def noEffect[K, V]: OnKeyValueStoreChange[K, V] = _ => ()
+}
+
+/**
+  * A subscriber actor that receives messages from the key value store whenever a change occurs.
+  *
+  * @param onChange the method that gets triggered when a change to key value store occurs
+  * @tparam K the key type
+  * @tparam V the value type
+  */
+class KeyValueStoreSubscriber[K, V] private (onChange: OnKeyValueStoreChange[K, V]) extends Actor {
+
+  private val key      = LWWMapKey[K, V](self.path.name)
+  private var previous = Map.empty[K, V]
+
+  private def diff(recent: Map[K, V]): KeyValeStoreChanges[K, V] = {
+    val added   = (recent -- previous.keySet).map { case (k, v) => ValueAdded(k, v) }.toSet
+    val removed = (previous -- recent.keySet).map { case (k, v) => ValueRemoved(k, v) }.toSet
+
+    val modified = (recent -- added.map(_.key)).foldLeft(Set.empty[KeyValeStoreChange]) {
+      case (acc, (k, v)) =>
+        previous.get(k).filter(_ == v) match {
+          case None => acc + ValueModified(k, v)
+          case _    => acc
+        }
+    }
+    KeyValeStoreChanges(added ++ modified ++ removed)
+  }
+
+  override def receive: Receive = {
+    case c @ Changed(`key`) =>
+      val map     = c.get(key).entries
+      val changes = diff(map)
+      if (changes.values.nonEmpty) onChange(changes)
+      previous = map
+
+    case _ => // drop
+  }
+}
+
+object KeyValueStoreSubscriber {
+
+  /**
+    * Enumeration of types related to changes to the key value store.
+    */
+  sealed trait KeyValeStoreChange extends Product with Serializable
+  object KeyValeStoreChange {
+
+    /**
+      * Signals that an element has been added to the key value store.
+      *
+      * @param key   the key
+      * @param value the value
+      */
+    final case class ValueAdded[K, V](key: K, value: V) extends KeyValeStoreChange
+
+    /**
+      * Signals that an already existing element has been updated from the key value store.
+      *
+      * @param key   the key
+      * @param value the value
+      */
+    final case class ValueModified[K, V](key: K, value: V) extends KeyValeStoreChange
+
+    /**
+      * Signals that an already existing element has been removed from the key value store.
+      *
+      * @param key   the key
+      * @param value the value
+      */
+    final case class ValueRemoved[K, V](key: K, value: V) extends KeyValeStoreChange
+  }
+
+  /**
+    * The set of changes that have occurred on the primary store
+    *
+    * @param values the set of changes
+    */
+  final case class KeyValeStoreChanges[K, V](values: Set[KeyValeStoreChange])
+
+  /**
+    * Constructs the [[KeyValueStoreSubscriber]] actor.
+    *
+    * @param id       the ddata key
+    * @param onChange the method that gets triggered whenever a change to the key value store occurs
+    * @tparam K the key type
+    * @tparam V the value type
+    * @return an [[ActorRef]] of the [[KeyValueStoreSubscriber]] actor
+    */
+  final def apply[K, V](id: String, onChange: OnKeyValueStoreChange[K, V])(implicit as: ActorSystem): ActorRef =
+    as.actorOf(Props(new KeyValueStoreSubscriber(onChange)), id)
+}
